8d80ca333f6678e8cf02ed0254d0148c8b93c971
Nishanth Raj T <87271636+t-nishanthraj@users.noreply.github.com>
2022-09-26 12:12:06 +0530

    Add files via upload

---
+++ ASSIGNMENT2/CHANDEESH/ASSIGNMENT2.py
@@ -0,0 +1,31 @@
+import random
+temp = []
+humidity = []
+
+temp_threshold = 75
+humidity_threshold= 75
+for i in range(100):
+    tempx = random.uniform(30, 105)
+    temp.append(tempx)
+    humidityx = random.uniform(30, 105)
+    humidity.append(humidityx)
+print("TEMP VALUES")
+print("****")
+for i in range(len(temp)):
+        if(temp[i]>temp_threshold):
+            print("Alarm : ", temp[i])
+        else:
+            print('Safe: ' , temp[i])
+print("****")
+print("****")
+print("HUMIDITY VALUES")
+print("****")
+for i in range(len(humidity)):
+    if (humidity[i] > humidity_threshold):
+        print('Alarm: ', temp[i])
+    else:
+        print('Safe: ' , temp[i])
+print("****")
+print("****")
+#print(humidity)
+#print(temp)
